e17b05a6f7742c3e3373c3ee984612f6f55a5835
Vyacheslav <dobrov.slava@yandex.ru>
2022-03-02 18:48:26 +0300

    Added solution of the second task. There was execution checking that first number is or isn't square of the second number

---
+++ 002Task/CompareTwoNumbers.py
@@ -0,0 +1,10 @@
+# По двум заданным числам проверять является ли первое квадратом второго
+
+print('Input first number')
+a = int(input())
+print('Input second number')
+b = int(input())
+if(a==b**2):
+    print('First number is square of second number')
+else:
+    print('First number is not square of second number')
