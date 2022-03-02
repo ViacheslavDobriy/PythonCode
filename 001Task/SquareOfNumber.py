0efbf8eece9ad0dc380913376c4084f39dec9f0c
Vyacheslav <dobrov.slava@yandex.ru>
2022-03-02 18:38:35 +0300

    Add first task and desscription of main function in PYTHON

---
+++ 001Task/SquareOfNumber.py
@@ -0,0 +1,8 @@
+# Вывести квадрат числа
+
+from asyncio.windows_events import NULL
+
+print ('Input your number')
+userNumber = int(input())
+squareOfNumber = userNumber*userNumber
+print ('Square of the user number is {}'.format(squareOfNumber))
