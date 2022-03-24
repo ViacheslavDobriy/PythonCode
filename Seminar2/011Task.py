5b0859271f7800b913ac127dc24e090a2b47cfe1
Vyacheslav <dobrov.slava@yandex.ru>
2022-03-24 19:51:33 +0300

    Fix problem, when user input 0

--- Seminar2/011Task.py
+++ Seminar2/011Task.py
@@ -3,9 +3,12 @@
 
 length = int(input('How many numbers you want to see?'))
 i = 1
-list = [1]
-while i < length:
-    list.append(list[i-1]*(-3))
-    i = i + 1
-print(list)
+if length > 0:
+    list = [1]
+    while i < length:
+        list.append(list[i-1]*(-3))
+        i = i + 1
+    print(list)
+else: 
+    print("Don't you want to see my list? It's a pity..")
 
