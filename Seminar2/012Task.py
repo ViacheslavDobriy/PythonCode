dedb8d29555324344cd7f953b43cbcddc124adf2
Vyacheslav <dobrov.slava@yandex.ru>
2022-03-31 18:30:24 +0300

    Added 012 Task, i used dictionary for make solution for this task

--- Seminar2/012Task.py
+++ Seminar2/012Task.py
@@ -2,7 +2,7 @@
 # Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
 userNumber = int(input('How many numbers do you wanna see?'))
-dictionary_index_value = []
+dictionary_index_value = {}
 for i in range(1,userNumber+1):
-    dictionary_index_value.append(3 * i + 1)
-# print(dictionary_index_value)
+    dictionary_index_value[i] = i * 3 + 1
+print(dictionary_index_value)
