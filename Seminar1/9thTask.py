88d936cde9872e20d42d32f137f94f3e0ec0040d
Vyacheslav <dobrov.slava@yandex.ru>
2022-03-31 09:58:20 +0300

    Added round for beatiful view

--- Seminar1/9thTask.py
+++ Seminar1/9thTask.py
@@ -10,4 +10,4 @@ point2Y = int(input('Enter Y coordinate of point'))
 long = point1X-point2X
 width = point1Y-point2Y
 distance = pow(long*long + width*width,0.5)
-print('{} is distance between first point ({},{}) and second point ({},{})'.format(distance, point1X, point1Y, point2X, point2Y))
+print('{} is distance between first point ({},{}) and second point ({},{})'.format(round(distance), point1X, point1Y, point2X, point2Y))
