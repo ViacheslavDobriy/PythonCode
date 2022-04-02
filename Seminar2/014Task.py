8599f0be668e9ebc8200dea8669de0da11ebdc15
Vyacheslav <dobrov.slava@yandex.ru>
2022-04-02 19:37:21 +0300

    Added comments to my functions

--- Seminar2/014Task.py
+++ Seminar2/014Task.py
@@ -1,6 +1,7 @@
 # Подсчитать сумму цифр в вещественном числе.
 
 def sum_of_digits(number):
+    '''Counting sum of all digits from the list'''
     sum = 0
     for i in number:
         while i >= 1:
@@ -9,6 +10,7 @@ def sum_of_digits(number):
     return sum
 
 def is_it_negative(number):
+    '''I'm checking that first number of user' number is positive. If it's not positive i will do that'''
     if number[0] < 0:
         number[0] *= -1
     return number
