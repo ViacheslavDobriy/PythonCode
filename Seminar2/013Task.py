3fe37d67d9a9e4691cd40717f8e6cf645ae2ba79
Vyacheslav <dobrov.slava@yandex.ru>
2022-04-02 15:47:15 +0300

    Deleted extra rows of code

--- Seminar2/013Task.py
+++ Seminar2/013Task.py
@@ -1,8 +1,5 @@
 # Пользователь задаёт две строки. Определить количество вхождений одной строки в другой.
 
-from tokenize import Number
-
-
 def Number_Of_Occurrences(str1, str2):
     result = 0
     for x in range(0, len(str1)):
@@ -17,8 +14,4 @@ user_string2 = list(input('Insert 2nd string').split())
 print(user_string1)
 print(user_string2)
 
-print(f'{Number_Of_Occurrences(user_string1, user_string2)} times first string have the same words like in second string')
-# list(map(int, input('Insert 1st string')))
-
-
-                
+print(f'{Number_Of_Occurrences(user_string1, user_string2)} times first string have the same words like in second string')                
