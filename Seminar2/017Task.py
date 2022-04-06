d32a9493e13e9828ed623c52f02312fe82a7aef2
Vyacheslav <dobrov.slava@yandex.ru>
2022-04-06 21:51:53 +0300

    Added solution of 017Task. I used three function. First is for creating list with random numbers, second one is for receiving indexes of elements from file.txt, and the last one is for multipliing elements. Last function have two arguements: both are the lists.

--- Seminar2/017Task.py
+++ Seminar2/017Task.py
@@ -2,18 +2,30 @@
 # Найти произведение элементов на указанных позициях.
 # Позиции хранятся в файле file.txt в одной строке одно число.
 
+import random
+
+def Random_range(number):
+    '''Generate new list with N numbers. It can have numbers from range [-user_numbers, user_numbers]'''
+    list = []
+    for i in range(userNumber):
+        list.append(random.randrange(-userNumber, userNumber))
+    print(list)
+    return list
+
+def Take_index():
+    '''Function take indexes from file and save elements in the new list'''
+    index_row = []
+    with open('C:/Users/user/Desktop/GeekBrains/Python/Seminar2/file.txt', 'r') as data: 
+        for line in data:
+            index_row.append(int(line))
+    return index_row
+
+def Multiply(first, second):
+    '''Function returns result of multipliing all elements with indexes from file'''
+    result = 1
+    for i in second:
+        result *= first[i]
+    return result
+
 userNumber = int(input('Input any number.'))
-list = []
-i = 0
-for i in range(-userNumber, userNumber+1,2):
-    list.append(i)
-result = 1
-with open('C:/Users/user/Desktop/GeekBrains/Python/Seminar2/file.txt', 'r') as data: 
-    for line in data:
-        temple = int(line)
-        result*= list[temple]
-print(list)
-print(result)
-
-
-
+print(Multiply(Random_range(userNumber), Take_index()))
