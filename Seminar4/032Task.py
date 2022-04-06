d5b6a78784c4f071aad96b8eb794ba3549a57d89
Vyacheslav <dobrov.slava@yandex.ru>
2022-04-06 21:53:23 +0300

    Deleted extra comments

--- Seminar4/032Task.py
+++ Seminar4/032Task.py
@@ -5,16 +5,4 @@ list2 = [1, 2, 3, 5, 1, 5, 3, 10]
 
 number = list(set(list2))
 print(number)
-# list1 = list(number)
-# print(list1)
-# result_list = []
-# digit = list1[0]
-# for i in list1:
-#     result_list.append(digit)
-
-# my_list = [1, 2, 3, 5, 1, 5, 3, 10]
-# my_set = set(my_list)
-# print(my_set)
-# my_list2 = list(my_set)
-# print(my_list2)
 
