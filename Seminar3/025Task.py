bf88967031938317dd9afed456a146dbedf1de68
Vyacheslav <dobrov.slava@yandex.ru>
2022-05-06 20:26:38 +0300

    Update 025 Task. Added functions and documentations.

--- Seminar3/025Task.py
+++ Seminar3/025Task.py
@@ -2,22 +2,21 @@
 
 number = 14
 
+
 def binary_func(number):
+    '''Function take int number and make binary number of it. It returns list that consists from 0 and 1'''
     binary_number = []
     while number >= 1:
-        binary_digit = number%2
+        binary_digit = number % 2
         number = number // 2
         binary_number.append(str(binary_digit))
     return binary_number
-result = binary_func(number)
-print(result)
-result.reverse()
-print(result)
-print(''.join(result))
 
 
-# i = 1
-# while i <= -len(binary_number):
-#     new_line.append(binary_number[-i])
-#     i = i - 1
-# print(new_line)
+def reverse_list(list_of_0_and_1):
+    '''Function take list of 0 and 1 and return inverted list'''
+    list_of_0_and_1.reverse()
+    return ''.join(list_of_0_and_1)
+
+
+print(f'{number} in binary is {reverse_list(binary_func(number))}')
