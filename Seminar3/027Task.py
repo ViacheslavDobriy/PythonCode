7cb54c00730a9ff4e1b8e82308405dabfe829a76
Vyacheslav <dobrov.slava@yandex.ru>
2022-05-07 09:42:56 +0300

    Update 027 Task. Added functions and documentations.

--- Seminar3/027Task.py
+++ Seminar3/027Task.py
@@ -1,39 +1,44 @@
 # Строка содержит набор чисел. Показать большее и меньшее число
 # Символ-разделитель - пробел
 
-# string_of_numbers = []
 
-user_number = str(input('Insert numbers'))
+def insert_number():
+    '''Function let user to insert row of numbers, and returns string value'''
+    return str(input('Insert numbers'))
+
 
 def string_int(user_number):
+    '''Function take string value like an argument and modify it in list. It returns list of integer values'''
     return [int(x) for x in user_number.split(' ')]
-string_list = string_int(user_number)
-print(string_list)
-min=min(string_list)
-max=max(string_list)
-print(f'{max} {min}')
-# def Maximum(string):
-#     max = string_of_numbers[0]
-#     for i in range(0,len(string_of_numbers)):
-#         if string_of_numbers[i] > max:
-#             max = string_of_numbers[i]
-#     return max
 
-# def Minimum(string_of_numbers):
-#     min = string_of_numbers[0]
-#     for i in range(0,len(string_of_numbers)):
-#         if string_of_numbers[i] < min:
-#             min = string_of_numbers[i]
-#     return min
 
-# max_str = str(Maximum(string_of_numbers))
-# min_str = str(Minimum(string_of_numbers))
+def Maximum(string):
+    '''Functon have only one argument - list of integer values. Here program is looking for maximum and returns it'''
+    max = string[0]
+    for i in range(0, len(string)):
+        if string[i] > max:
+            max = string[i]
+    return max
 
-# def Create_Row(max_str, min_str):
-#     result = []
-#     result.append(max_str)
-#     result.append(' ')
-#     result.append(min_str)
-#     return result
-    
-# print(''.join(Create_Row(max_str, min_str)))
+
+def Minimum(string_of_numbers):
+    '''Functon have only one argument - list of integer values. Here program is looking for minimum and returns it'''
+    min = string_of_numbers[0]
+    for i in range(0,len(string_of_numbers)):
+        if string_of_numbers[i] < min:
+            min = string_of_numbers[i]
+    return min
+
+def Create_Row(max_str, min_str):
+    '''Function is getting two int values - minimum and maximum and create a list which consists of these two values and space between. '''
+    result = []
+    result.append(max_str)
+    result.append(' ')
+    result.append(min_str)
+    return result
+
+
+string_of_numbers = string_int(insert_number())
+max_str = str(Maximum(string_of_numbers))
+min_str = str(Minimum(string_of_numbers))
+print(''.join(Create_Row(max_str, min_str)))
