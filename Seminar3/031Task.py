999cf724ca1fe6b584c9920719eaa25d527e0ede
Vyacheslav <dobrov.slava@yandex.ru>
2022-05-08 20:44:05 +0300

    Update 031 Task. Was added checking of inserted numbers, functions and documentation.

--- Seminar3/031Task.py
+++ Seminar3/031Task.py
@@ -1,29 +1,29 @@
 # Составить список простых множителей натурального числа N
 # 36 = 2*2*3*3
-number = int(input('Insert number'))
 
-list = []
 
-for i in range(2,number):
-    while number %i == 0:
-        number/= i
-        list.append(i)
-print(list)
+def inserting_number():
+    '''Function is inviting user to insert number. It returns inserted string'''
+    return input('Insert number')
 
-# import math 
 
-# num = int(input('Введите число: ')) 
-# def faktorizaciya(num): 
-     
-#     while num % 2 == 0: 
-#         print(2) 
-#         num = num / 2 
- 
-#     for i in range(3, int(math.sqrt(num)) + 1, 2): 
- 
-#         while num % i == 0: 
-#             print(i) 
-#             num = num / i 
-#     if num > 2: 
-#         print(num)   
-# faktorizaciya(num)
+def check_int(number):
+    '''Function for checking inserted number. It accepts value, and if it's int it will be return, else this function will be called again.'''
+    try:
+        return int(number)
+    except:
+        print("Illegal number. Try one more time")
+        return check_int(inserting_number())
+
+
+def prime_factors(number):
+    '''Function make list of simple factors. It has integer argument and returns list'''
+    factors_list = []
+    for i in range(2, number):
+        while number % i == 0:
+            number /= i
+            factors_list.append(i)
+    return factors_list
+
+
+print(prime_factors(check_int(inserting_number())))
