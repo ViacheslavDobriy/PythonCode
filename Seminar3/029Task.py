4024da330c5aa14facb7d6a0e588253058f49ea5
Vyacheslav <dobrov.slava@yandex.ru>
2022-05-08 19:52:31 +0300

    Update 029 Task. Was added checking of inserted numbers and documentation.

--- Seminar3/029Task.py
+++ Seminar3/029Task.py
@@ -1,14 +1,31 @@
 # Найти НОК двух чисел
 
-user_number1 = int(input('Insert number 1'))
-user_number2 = int(input('Insert number 2'))
+
+def insert_number():
+    '''Function for inserting number. Don't have argument, and returns inserting integer number'''
+    return input('Insert number: ')
+
+
+def check_int(number):
+    '''Function for checking inserted number. It accepts value, and if it's int it will be return, else this function will be called again.'''
+    try:
+        return int(number)
+    except:
+        print("Illegal number. Try one more time")
+        return check_int(insert_number())
+
 
 def nod(x1, x2):
+    '''Function that definite NOD of two numbers. It has two integer arguments. And returns NOD'''
     if x2 == 0:
         return x1
     else:
         return nod(x2, x1%x2)
-print(nod(user_number1, user_number2))
+
+
 def nok(x1, x2):
+    '''This function definites NOK of two numbers. It has two integer arguments and returns NOK - integer number'''
     return x1*x2//nod(x1,x2)
-print(nok(user_number1, user_number2))
+
+
+print(nok(check_int(insert_number()), check_int(insert_number()))) # there is calling all functions
